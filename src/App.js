c6d680cb9ddab82137f08288788dda945916067d
unknown <user@SRMLAP406.srmtchn.co.in>
2022-11-07 18:21:19 +0530

    list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/login.js';
+import Home from './components/home.js';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route path="/home.js" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+    );
 }
 
 export default App;
--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import axios from "axios";
+//import Home from "./home.js";
 
 
 class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={name:"",id:"",result:[]};
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = {
+            id: this.state.id,
+            name: this.state.name
+        }
+        console.log(this.state.id);
+        var userid=this.state.id;
+        var username=this.state.name;
+        axios.get('https://jsonplaceholder.typicode.com/users')
+          .then(res=>{
+            //console.log(res.data);
+            var x=res.data
+            // x.map(name => (  
+            //     console.log(name); 
+            // ))
+            var isset_data="";
+            x.forEach((el)=>{
+                if(el.id==userid && el.name==username){
+                    isset_data=1;
+                }
+            })
+            if(isset_data==1){
+                //alert("Logged In Successfully!");
+                window.open('./home.js');
+            }else{
+                alert("User Name and UserId mismatched!");
+            }
+          }).catch((err)=>{
+            console.log(err);
+            alert("notsubmit");
+          })
+      }
+    
+      handleChange = event =>{
+        let nam= event.target.name;
+        let val= event.target.value;
+        this.setState({[nam]:val});
+      }
     render(){
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Login Form</h2>
                 <div>
                     <label for="uname"><b>Username: </b></label>
@@ -17,6 +62,7 @@ class Login extends React.Component{
                 <div>
                     <button type="submit">Login</button>
                 </div>
+                <p className="login-error">{this.state.errormessage}</p>
                 <label>
                     <input type="checkbox" checked="checked" name="remember" /> Remember me
                 </label>
